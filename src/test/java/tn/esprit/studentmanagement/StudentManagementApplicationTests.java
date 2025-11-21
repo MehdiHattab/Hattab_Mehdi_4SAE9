0b52c6166923dc7666af1695ff30426269a79672
zakariabouchaddakh <zakariazikou513@gmail.com>
2025-11-21 16:44:38 +0100

    s

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,10 +2,10 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestComponent;
 import org.springframework.test.context.ActiveProfiles;
 
-@TestComponent
+@SpringBootTest
+@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
